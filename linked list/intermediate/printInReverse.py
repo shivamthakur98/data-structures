fab6784bd613ade1843b4af88ff7cecdb5e8d236
shivam <shivam98thakur@gmail.com>
2023-06-15 09:59:57 +0530

    Added code to print the LL in reverse

---
+++ linked list/intermediate/printInReverse.py
@@ -0,0 +1,48 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+def takeInput()->Node:
+    datas = [int(el) for el in input().strip().split(" ")]
+    head, tail = None, None
+    for data in datas:
+        if data == -1:
+            break
+        # create Node from the data
+        newNode: Node = Node(data)
+
+        # intializing head and tail
+        # if head is already intialized, create links
+        if head == None:
+            head = newNode
+            tail = newNode
+        else:
+            tail.next = newNode
+            tail = newNode
+
+    #return starting point of the Linked List
+    return head
+
+def printLL(head):
+    currNode = head
+    while currNode != None:
+        print(str(currNode.data) + "->", end="")
+        currNode = currNode.next
+    print("None")
+
+def printInReverse(head):
+    if head == None:
+        return
+
+    printInReverse(head.next)
+    print(head.data, end = " ")
+    return
+
+no_of_test_cases = int(input("Enter no. of test-cases: "))
+
+while no_of_test_cases > 0:
+    head = takeInput()
+    printInReverse(head)
+    no_of_test_cases -= 1
+ 

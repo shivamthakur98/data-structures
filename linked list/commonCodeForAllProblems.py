50a40fde1afffb32982a66202fe2224928c4a979
shivam <shivam98thakur@gmail.com>
2023-06-15 16:31:16 +0530

    Added code for some problems related to Linked List

---
+++ linked list/commonCodeForAllProblems.py
@@ -0,0 +1,40 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+def takeInput()->Node:
+    datas = [int(el) for el in input().strip().split(" ")]
+    head, tail = None, None
+    for data in datas:
+        if data == -1:
+            break
+        # create Node from the data
+        newNode: Node = Node(data)
+
+        # intializing head and tail
+        # if head is already intialized, create links
+        if head == None:
+            head = newNode
+            tail = newNode
+        else:
+            tail.next = newNode
+            tail = newNode
+
+    #return starting point of the Linked List
+    return head
+
+def printLL(head):
+    currNode = head
+    while currNode != None:
+        print(str(currNode.data) + "->", end="")
+        currNode = currNode.next
+    print("None")
+
+no_of_test_cases = int(input("Enter no. of test-cases"))
+
+while no_of_test_cases > 0:
+    head = takeInput()
+    printLL(newHead)
+
+    no_of_test_cases -= 1

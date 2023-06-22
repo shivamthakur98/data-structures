247255cc4f5489c4aca34b125200381d621a147d
shivam <shivam98thakur@gmail.com>
2023-06-22 16:26:17 +0530

    Added code for creating stack using inbuild python module

---
+++ stack/stackUsingPythonInbuild.py
@@ -0,0 +1,45 @@
+from stackUsingArr import Stack
+from stackUsingLL import Stack as StackLL
+from queue import Queue
+
+'''
+    Stack using Array
+'''
+print("Stack operation using Array")
+stack = Stack()
+print(stack.size())
+stack.push(1)
+stack.push(2)
+stack.push(3)
+
+while stack.isEmpty() == False:
+    print(stack.pop())
+
+'''
+    Stack using Linked List
+'''
+print("Stack operation using LL")
+stack = StackLL()
+print(stack.size())
+stack.push(1)
+stack.push(2)
+stack.push(3)
+
+while stack.isEmpty() == False:
+    print(stack.pop())
+
+'''
+    Stack using Lifo Queue
+    Python has default implementation for the Queue's which are FIFO data-structure
+    It also offers LIFO Queue which is nothing but stack implementation
+'''
+print("Stack operation using Python's default stack")
+stack = Queue()
+stack.put(1)
+stack.put(2)
+stack.put(3)
+
+while stack.empty() == False:
+    print(stack.get())
+
+
